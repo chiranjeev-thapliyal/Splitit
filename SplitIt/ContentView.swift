a838328b75d685ac09bc82eac52b843597daee8e
Chiranjeev Thapliyal <6258860@gmail.com>
2024-04-09 02:15:32 +0530

    Initial Commit

---
+++ SplitIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitIt
+//
+//  Created by Chiranjeev Thapliyal on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitIt/SplitItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitItApp.swift
+//  SplitIt
+//
+//  Created by Chiranjeev Thapliyal on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
