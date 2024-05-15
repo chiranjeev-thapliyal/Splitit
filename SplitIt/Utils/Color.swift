26beb7fab73b68a3737d74330b327b0715ff72dc
Chiranjeev Thapliyal <6258860@gmail.com>
2024-05-15 10:15:23 +0530

    feat: changed appIcon

--- SplitIt/Utils/Color.swift
+++ SplitIt/Utils/Color.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 extension Color {  
-    static let darkGreen = Color(red: 15 / 255, green: 140 / 255, blue: 110 / 255) // 1 touch lighter as requested before
+    static let darkGreen = Color(red: 15 / 255, green: 140 / 255, blue: 110 / 255)
     static let regularGreen = Color(red: 30 / 255, green: 180 / 255, blue: 150 / 255)
     static let lightGreen = Color(red: 95 / 255, green: 200 / 255, blue: 180 / 255)
     static let crimson = Color(red: 1, green: 0.8, blue: 0)
